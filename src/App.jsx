a052d156c5faa54457f856c870e3975fcc42c004
Programming Hero <programming.hero1@gmail.com>
2023-09-09 21:47:30 -0500

    project setup with tailwind css

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+      <h1 className='text-6xl bg-red-300'>Knowledge Cafe</h1>
+      
+      
+    </>
+  )
+}
+
+export default App
